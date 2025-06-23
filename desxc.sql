6d3ad9713ba0e53ac83a4191db457aae540ed072
RasikaThool <rashiwankhede@gmail.com>
2025-06-24 00:56:15 +0530

    Add files via upload

---
+++ desxc.sql
@@ -0,0 +1,29 @@
+select * from portfolio_projects.nashville housing data for data cleaning (reuploaded);`nashville housing data for data cleaning (reuploaded)``nashville housing data for data cleaning (reuploaded)``nashville housing data for data cleaning (reuploaded)`
+show databases;
+use portfolio_projects;
+show tables;
+select * from ;
+drop table customer_records;customer_records_data
+
+alter table `nashville housing data for data cleaning (reuploaded)`
+rename NashvilleHousing;
+datacleaning_nashvilleselect * from NashvilleHousing;
+select count(*) from NashvilleHousing;
+drop table NashvilleHousing;
+show tables;
+select * from datacleaning_nashvillehousing;
+select count(*) from datacleaning_nashvillehousing;
+select * from nashvillehousing_datacleaning;
+select count(*) from nashvillehousing_datacleaning;
+drop table nashvillehousing_datacleaning;
+show tables;customer_records
+select * from datacleaning_nashville;
+select count(*) from datacleaning_nashville;
+alter table datacleaning_nashville housing data change datacleaning; 
+select * from datacleaning_nashville housing data;
+`datacleaning_nashville housing data`
+datacleaning_nashvilledatacleaning_nashville
+
+
+
+
